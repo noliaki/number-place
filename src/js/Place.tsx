3f9c20269062273395a083d2d935c4796d0597f9
noliaki <noliaki@gmail.com>
2019-10-29 18:23:26 +0900

    [wip]

--- src/js/Place.tsx
+++ src/js/Place.tsx
@@ -2,23 +2,40 @@ import React, { useState, useRef, useMemo } from 'react'
 import Cell from './Cell'
 
 export default (): JSX.Element => {
-  const [size, setSize] = useState(3)
+  const [size, setSize]: [
+    number,
+    React.Dispatch<React.SetStateAction<number>>
+  ] = useState(3)
 
-  const numbers = useMemo(() => {
-    const arr: number[] = []
+  const side: number = useMemo<number>((): number => Math.pow(size, 2), [size])
 
-    for (let i: number = 0, len: number = Math.pow(size, 2); i < len; i++) {
-      arr[i] = i
+  const numbers = useMemo<(number | null)[]>((): (number | null)[] => {
+    const arr: (number | null)[] = []
+
+    for (let i: number = 0, len: number = Math.pow(side, 2); i < len; i++) {
+      arr[i] = null
     }
 
     return arr
-  }, [size])
+  }, [size, side])
 
   return (
     <>
+      <header>
+        <input
+          type="number"
+          name="num"
+          id="num"
+          value={size}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>): void => {
+            event.stopPropagation()
+            setSize(parseInt(event.currentTarget.value, 10))
+          }}
+        />
+      </header>
       {numbers.map(
-        (val: number, i: number): JSX.Element => (
-          <Cell index={val} key={i} />
+        (val: number | null, i: number): JSX.Element => (
+          <Cell key={i} index={val} />
         )
       )}
     </>
